97fd8d934be2fcdd2a274cea23cdecb59c93fe5b
mumudeveloper <mumudeveloper@outlook.com>
2019-02-12 11:32:34 +0800

    init.

---
+++ config/config.default.js
@@ -0,0 +1,10 @@
+'use strict';
+
+/**
+ * egg-mumuimp-userservice default config
+ * @member Config#mumuimpUserservice
+ * @property {String} SOME_KEY - some description
+ */
+exports.mumuimpUserservice = {
+
+};
